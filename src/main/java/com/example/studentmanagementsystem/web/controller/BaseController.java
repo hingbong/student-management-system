c97c0cdf0e9a739922525332918a11d81439abd6
Hingbong Lo <hingbonglo@gmail.com>
2019-05-26 18:20:51 +0800

    remove HttpHeadersConfig
    Signed-off-by: hingbong <hingbonglo@gmail.com>

--- src/main/java/com/example/studentmanagementsystem/web/controller/BaseController.java
+++ src/main/java/com/example/studentmanagementsystem/web/controller/BaseController.java
@@ -2,12 +2,10 @@ package com.example.studentmanagementsystem.web.controller;
 
 import com.example.studentmanagementsystem.exception.OperationException;
 import com.example.studentmanagementsystem.util.JsonResult;
-import java.net.URI;
+import java.io.IOException;
 import java.time.LocalDateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,19 +13,16 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /** @author Hingbong */
 public abstract class BaseController {
 
-    private HttpHeaders emptyHeader;
-
-    @RequestMapping
-    public ResponseEntity gotoIndex() {
-        emptyHeader.setLocation(URI.create("index.html"));
-        return new ResponseEntity(emptyHeader, HttpStatus.FOUND);
-    }
+  @RequestMapping
+  public void gotoIndex(HttpServletResponse response) throws IOException {
+    response.sendRedirect("index.html");
+  }
 
   @ExceptionHandler
   @ResponseStatus(HttpStatus.OK)
   public JsonResult<String> handleException(final Throwable e) {
     String result;
-      System.err.println(LocalDateTime.now() + "=>" + e.getMessage());
+    System.err.println(LocalDateTime.now() + "=>" + e.getMessage());
     if (e instanceof NullPointerException) {
       result = "操作错误！尝试使用的对象为Null！";
     } else if (e instanceof OperationException) {
@@ -35,11 +30,6 @@ public abstract class BaseController {
     } else {
       result = "操作错误！请联系系统管理员！";
     }
-      return JsonResult.newJson(0, result);
-  }
-
-    @Autowired
-    public void setEmptyHeader(HttpHeaders emptyHeader) {
-        this.emptyHeader = emptyHeader;
+    return JsonResult.newJson(0, result);
   }
 }
